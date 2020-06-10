b5a2d50abbb5761ec799632b060ad53a85fc3295
juanmartinez <juanm.jm49@gmail.com>
2020-06-10 16:38:16 -0700

    course contents step7/8

--- src/index.js
+++ src/index.js
@@ -141,11 +141,14 @@ const Statistics = (props) => {
 }
 
 const Content = (props) => {
+    let nums = props.content.map(part => part.exercises);
     return (
         <div>
             {props.content.map(part =>
                 <Part key={part.id} part={part.name} exercises={part.exercises} />
-                )}
+                )
+            }
+            <p><strong>total of {nums.reduce((a, b) => { return a + b; }, 0)} exercises</strong></p>
         </div>
 
     );
@@ -179,6 +182,11 @@ const App = (props) => {
                 name: 'State of a component',
                 exercises: 14,
                 id: 3
+            },
+            {
+                name: 'Redux',
+                exercises: 11,
+                id: 4
             }
         ]
     }
