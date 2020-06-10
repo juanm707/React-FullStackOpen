d10a249db86a73a0d44b13305c919cb291896615
juanmartinez <juanm.jm49@gmail.com>
2020-06-09 21:06:41 -0700

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,188 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = ({name, age}) => {
+    // const {name, age} = props;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    const yearBorn = () => new Date().getFullYear() - age;
+
+    return (
+        <div>
+            <p>
+                Hello {name}, you are {age} years old.
+            </p>
+            <p>
+                So you were probably born in {yearBorn()}.
+            </p>
+        </div>
+    );
+}
+
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = (props) => {
+    return (
+        <button onClick={props.handleClick} style={props.style}>
+            {props.text}
+        </button>
+    );
+}
+
+const Part = (props) => {
+    return (
+        <div>
+            <h2>{props.part}</h2>
+            <p>{props.exercises}</p>
+        </div>
+    );
+}
+
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.course.name}</h1>
+        </div>
+    );
+}
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
+            <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises}/>
+            <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises}/>
+        </div>
+    );
+}
+
+const Total = (props) => {
+    let total = 0;
+    console.log(props);
+    props.total.parts.forEach(value => {
+        total = total + value.exercises;
+    });
+
+    return (
+        <div>
+            <p>Total exercises: {total}</p>
+        </div>
+    );
+}
+
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        );
+    }
+
+    return (
+      <div>
+          button press history: {props.allClicks.join(' ')}
+      </div>
+    );
+}
+
+const App = (props) => {
+    // const course = {
+    //     name: 'Half Stack application development',
+    //     parts: [
+    //         {
+    //             name: 'Fundamentals of React',
+    //             exercises: 10
+    //         },
+    //         {
+    //             name: 'Using props to pass data',
+    //             exercises: 7
+    //         },
+    //         {
+    //             name: 'State of a component',
+    //             exercises: 14
+    //         }
+    //     ]
+    // }
+    //
+    // return (
+    //     <div>
+    //         <Header course={course} />
+    //         <Content course={course} />
+    //         <Total total={course}/>
+    //     </div>
+    // );
+
+    // const [counter, setCounter] = useState(0);
+    //
+    // const increaseByOne = () => setCounter(counter + 1);
+    // const setToZero = () => setCounter(0);
+    // const decreaseByOne = () => setCounter(counter - 1)
+
+    // const handleClick = () => {
+    //     setCounter(counter + 1);
+    // }
+
+    // setTimeout(
+    //     () => setCounter(counter + 1),
+    //     1000
+    // )
+
+    // return (
+    //     <div>
+    //         <Display counter={counter} />
+    //         <Button
+    //             handleClick={increaseByOne}
+    //             style={{backgroundColor: "limegreen"}}
+    //             text={"Add One"}
+    //         />
+    //         <Button
+    //             handleClick={setToZero}
+    //             style={{backgroundColor: "red"}}
+    //             text={"Reset to zero"}
+    //         />
+    //         <Button
+    //             handleClick={decreaseByOne}
+    //             style={{backgroundColor: "lightblue"}}
+    //             text={"Subtract One"}
+    //         />
+    //     </div>
+    // );
+
+    const[left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
+    const [allClicks, setAll] = useState([]);
+
+    const handleLeftClick = () => {
+        setAll(allClicks.concat('L'));
+        setLeft(left + 1);
+    }
+
+    const handleRightClick = () => {
+        setAll(allClicks.concat('R'));
+        setRight(right + 1);
+    }
+    
+    return (
+        <div>
+            <div>
+                {left}
+                <Button
+                    handleClick={handleLeftClick}
+                    style={{backgroundColor: "yellow"}}
+                    text={'left'}
+                />
+                <Button
+                    handleClick={handleRightClick}
+                    style={{backgroundColor: "green"}}
+                    text={'right'}
+                />
+                {right}
+            </div>
+            <div>
+                <History allClicks={allClicks} />
+            </div>
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
